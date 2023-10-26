cf1c6461b394309290f51a78ab0057c6f32e0ccb
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-10-26 14:45:48 -0400

    Fixed the issue with the login/logout buttons.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
     res.render('homepage', {
       comments,
-      logged_in: req.session.logged_in,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
